347a700390c596ff8e95a9d81c30d5030b04795a
Nandakishor2206 <nandakishorchandolu11@gmail.com>
2025-04-20 11:54:11 +0530

    Update product.controller.ts

--- backend/src/products/product.controller.ts
+++ backend/src/products/product.controller.ts
@@ -1,1 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
+
+@Controller('products')  // Define the route to handle requests to /products
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()  // Handle GET requests to /products
+  getAllProducts() {
+    return this.productService.findAll();
+  }
+}
 
