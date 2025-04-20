4b6db356647a8f9ca2e54ac35203a9a4c4ba3f8f
Nandakishor2206 <nandakishorchandolu11@gmail.com>
2025-04-20 11:50:07 +0530

    events.gateway.ts

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -1,1 +1,19 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from '../orders/order.entity';
+import { getSocketIO } from '../socket.io'; // import Socket helper
+
+@Injectable()
+export class EventsGateway implements OnModuleInit {
+  server!: Server; // Non-null assertion
+
+  onModuleInit() {
+    // Now you can safely assign `server` here
+    this.server = getSocketIO();
+  }
+
+  // Add your methods to handle socket events
+}
 
